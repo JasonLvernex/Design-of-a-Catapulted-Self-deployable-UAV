11c7c0036138ad8b8a42da20052905cac638d072
Lyu Tian <96185978+JasonLvernex@users.noreply.github.com>
2021-12-19 17:08:46 +0800

    Add files via upload

---
+++ STM32 program/Output/imu.d
@@ -0,0 +1,5 @@
+.\output\imu.o: MATH\imu.c
+.\output\imu.o: MATH\imu.h
+.\output\imu.o: .\USER\ALL_DATA.h
+.\output\imu.o: MATH\myMath.h
+.\output\imu.o: F:\mdk5\ARM\ARMCC\Bin\..\include\math.h
---
+++ STM32 program/Output/pid.d
@@ -0,0 +1,5 @@
+.\output\pid.o: MATH\pid.c
+.\output\pid.o: MATH\pid.h
+.\output\pid.o: .\USER\ALL_DATA.h
+.\output\pid.o: F:\mdk5\ARM\ARMCC\Bin\..\include\stdbool.h
+.\output\pid.o: MATH\myMath.h
